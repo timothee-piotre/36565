f8b39aed663e19b981f3b8e6b9a7ca0a5b8a4aa0
timothee-piotre <155533950+timothee-piotre@users.noreply.github.com>
2024-08-26 21:07:37 +0200

    Update and rename authController.js to authController(register).js

--- authController(register).js
+++ authController(register).js
@@ -21,24 +21,3 @@ exports.register = async (req, res) => {
         res.status(400).json({ message: 'User could not be created', error: error.message });
     }
 };
-
-exports.login = async (req, res) => {
-    const { email, password } = req.body;
-
-    try {
-        const user = await User.findOne({ where: { email } });
-
-        if (user && (await user.matchPassword(password))) {
-            res.json({
-                id: user.id,
-                username: user.username,
-                email: user.email,
-                token: generateToken(user.id),
-            });
-        } else {
-            res.status(401).json({ message: 'Invalid email or password' });
-        }
-    } catch (error) {
-        res.status(400).json({ message: 'Cannot log in', error: error.message });
-    }
-};
