3b24ee322e600b80e7327a5c8ad6ae9efb39db6a
timothee-piotre <155533950+timothee-piotre@users.noreply.github.com>
2024-08-26 21:05:23 +0200

    Update and rename authController.js to authController(login).js

--- authController(login).js
+++ authController(login).js
@@ -6,21 +6,6 @@ const generateToken = (id) => {
     return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: '30d' });
 };
 
-exports.register = async (req, res) => {
-    const { username, email, password } = req.body;
-
-    try {
-        const user = await User.create({ username, email, password });
-        res.status(201).json({
-            id: user.id,
-            username: user.username,
-            email: user.email,
-            token: generateToken(user.id),
-        });
-    } catch (error) {
-        res.status(400).json({ message: 'User could not be created', error: error.message });
-    }
-};
 
 exports.login = async (req, res) => {
     const { email, password } = req.body;
